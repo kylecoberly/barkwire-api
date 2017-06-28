7ab71ff432013edd1cf9429a5020d3ed3b7b0e86
Kyle Coberly <kyle.coberly@gmail.com>
2017-06-28 10:40:57 -0600

    Updating image links

--- test/crud.js
+++ test/crud.js
@@ -15,19 +15,19 @@ describe("It can CRUD dogs", function(){
                         id: 1,
                         name: "Charles Barkley",
                         description: "Fit, tan, single and ready to mingle. Up for a romantic walk this afternoon? I'm an adventurer who is looking for the right mate for endless playdates. I have exquisite taste, so if you're a foodie, call me.  We can chat about bones over the phone.",
-                        imageUrl: "images/dog1.jpg",
+                        imageUrl: "https://s3-us-west-2.amazonaws.com/assessment-images/barkwire/dog1.jpg",
                         imageCaption: "Me throwin' the bee around"
                     },{
                         id: 2,
                         name: "Bilbo Waggins",
                         description: "What's Valentine's Day without chocolate? I'm single, brown and a goofball looking for love. Let's plan a date made for two, or we can hang out with friends.",
-                        imageUrl: "images/dog2.jpg",
+                        imageUrl: "https://s3-us-west-2.amazonaws.com/assessment-images/barkwire/dog2.jpg",
                         imageCaption: "I'm into sports"
                     },{
                         id: 3,
                         name: "Helga Hufflepup",
                         description: "Warning: I'm a heartbreaker. But what princess isn't?  Falling in love with me is easy to do, so don't be surprised if you find yourself in a jewelry store shopping for princess-cut diamonds to throw at my feet. I'm looking for a prince to call my own.",
-                        imageUrl: "images/dog3.jpg",
+                        imageUrl: "https://s3-us-west-2.amazonaws.com/assessment-images/barkwire/dog3.jpg",
                         imageCaption: "My senior picture, lol"
                     }]
                 });
@@ -47,7 +47,7 @@ describe("It can CRUD dogs", function(){
                         id: 1,
                         name: "Charles Barkley",
                         description: "Fit, tan, single and ready to mingle. Up for a romantic walk this afternoon? I'm an adventurer who is looking for the right mate for endless playdates. I have exquisite taste, so if you're a foodie, call me.  We can chat about bones over the phone.",
-                        imageUrl: "images/dog1.jpg",
+                        imageUrl: "https://s3-us-west-2.amazonaws.com/assessment-images/barkwire/dog1.jpg",
                         imageCaption: "Me throwin' the bee around"
                     }
                 });
